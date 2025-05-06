2a3470210d0ff3c86d12db3f0fcc4151efd20984
Paul-Ayesiga <ayesigapo@gmail.com>
2025-05-06 14:10:11 +0300

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome');
+})->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    // Other authenticated routes...
+});
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
